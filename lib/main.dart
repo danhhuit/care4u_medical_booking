0f021f83cdc59e1ccb902232696ded2204a0e3b8
An <nguyenan250805@gmail.com>
2026-03-30 23:08:11 +0700

    feat: sua lai loi

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
-import 'features/doctors/screens/doctors_screen.dart';
-import 'features/appointments/screens/appointments_screen.dart';
-import 'features/specialties/screens/specialties_screen.dart'; 
+import 'navigation/main_navigation.dart'; 
 
 void main() {
   runApp(const MedicalApp());
 }
-
 class MedicalApp extends StatelessWidget {
   const MedicalApp({super.key});
   @override
@@ -14,67 +11,20 @@ class MedicalApp extends StatelessWidget {
     return MaterialApp(
       title: 'Care4U Medical',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        useMaterial3: true,
-        scaffoldBackgroundColor: Colors.white,
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.white,
-          foregroundColor: Colors.black,
-          elevation: 0,
-          centerTitle: true,
-        ),
-      ),
-      home: const MainNavigation(),
+      theme: _buildTheme(), 
+      home: MainNavigation(),
     );
   }
-}
-class MainNavigation extends StatefulWidget {
-  const MainNavigation({super.key});
-  @override
-  State<MainNavigation> createState() => _MainNavigationState();
-}
-class _MainNavigationState extends State<MainNavigation> {
-  int _selectedIndex = 0;
-  final List<Widget> _screens = [
-    const DoctorsScreen(),      
-    const SpecialtiesScreen(),  
-    const AppointmentsScreen(), 
-  ];
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: IndexedStack(
-        index: _selectedIndex,
-        children: _screens,
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.blue,
-        unselectedItemColor: Colors.grey,
-        type: BottomNavigationBarType.fixed, 
-        onTap: (index) {
-          setState(() {
-            _selectedIndex = index;
-          });
-        },
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person_search_outlined),
-            activeIcon: Icon(Icons.person_search),
-            label: 'Bác sĩ',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.grid_view_outlined),
-            activeIcon: Icon(Icons.grid_view_rounded),
-            label: 'Chuyên khoa',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.calendar_today_outlined),
-            activeIcon: Icon(Icons.calendar_today),
-            label: 'Lịch hẹn',
-          ),
-        ],
+  ThemeData _buildTheme() {
+    return ThemeData(
+      primarySwatch: Colors.blue,
+      useMaterial3: true,
+      scaffoldBackgroundColor: Colors.white,
+      appBarTheme: const AppBarTheme(
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        elevation: 0,
+        centerTitle: true,
       ),
     );
   }
