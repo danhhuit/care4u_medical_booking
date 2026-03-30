047feec4bebeb483f522f68251ad408af1ab6796
thiện <thienphan1207@gmail.com>
2026-03-30 19:40:38 +0700

    thien lam ho so kham

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
 import 'app/app.dart';
 import 'app/config/dependency_injection.dart';
+import 'features/medical_records/history_page.dart';
+import 'features/medical_records/profile_page.dart';  
+import 'features/medical_records/results_page.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await DependencyInjection.init();
-  runApp(const Care4uApp());
+  
+  // Comment dòng dưới lại khi muốn chạy toàn bộ app
+  // runApp(const Care4uApp());
+  
+  // Bỏ comment đoạn dưới để chạy thử trang HistoryPage
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: ProfilePage(),
+  ));
 }
