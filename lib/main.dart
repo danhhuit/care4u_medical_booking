5527ae492b5be58feeb6b7360bbb3b15460f5b7f
thiện <thienphan1207@gmail.com>
2026-03-30 23:35:25 +0700

    feat:thien lam tt y te

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,15 @@
 import 'package:flutter/material.dart';
 import 'app/app.dart';
 import 'app/config/dependency_injection.dart';
-import 'features/prescriptions/prescription_list_page.dart';
-import 'features/prescriptions/prescription_detail_page.dart';
-import 'features/prescriptions/revisit_schedule_page.dart';
+import 'features/health_center/health_center_page.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await DependencyInjection.init();
-  
 
 
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: PrescriptionDetailPage(),
+    home: HealthCenterPage(),
   ));
 }
-
-
